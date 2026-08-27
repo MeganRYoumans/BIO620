5eaf418f4b8695cd8bf401f3c75d7b20db0d7254
Megan Youmans <megan.r.youmans@gmail.com>
2026-08-27 17:45:24 -0400

    Lab Basics 1 from BIO620
    Problem set workthrough of Lab Basics 1

---
+++ Lab Basics 1 Megan Youmans.R
@@ -0,0 +1,76 @@
+## Megan Youmans, Lab 1 R Basics
+## Q1
+12 %% 5
+## Q1 Remainder = 2
+## Q2
+x <- 1:10,2
+y <- x^-1,3
+plot(x,y)
+## Q3
+## First selects 1-5 using 1:5, sets a repeat (3,5) repeat 3 times to 5
+## Seond selects 1-5 using 1:5, sets repeat at (1:5) so 1 is produced one time, 2 is produced two times, 3 three, 4 four, and 5 five. Setting how many times to repeat the number in the sequence
+## Third selects 1-5 using 1:5, repeats it at c() interval as (1,2,3,2,1) so 1 is one time, 2 is two times, 3 is three times, 4 is two times, 5 is one time.
+## Fourth sets the letters of "A", "B", "C", including quotations then sets the repeat interval of (4,3,2) so "A" four times, "B" three times, "C" two times.
+## Q4
+y <- seq(0,100,2)
+y
+sum(y)
+mean(y)
+sd(y)
+var(y)
+median(y)
+max(y)
+min(y)
+summary(y)
+## Q5 - Have to set the logic for what numbers will correspond to which request
+y <- c(8,3,5,7,6,6,8,9,2,3,9,4,10,4,11)
+numbers <- y
+is_even <- numbers %% 2 == 0
+is_even
+even <- which(is_even)
+even
+is_odd <- numbers %%2 !=0
+is_odd
+odd <- which(is_odd)
+odd
+## Q6
+## Q6 A
+400/17
+## Q6 B - note remember to use * for multiplication 
+12 * (6*15)/(40/21)
+## Q6 C - for sq root can use sqrt() or for cubed or more turn into fraction and muliply to power
+250^(1/3)
+## Q6 D
+log10(1000)
+## Q7 - use rep(c) instead of seq for string sequence 
+seq(0,16,4)
+seq(0.3,1.5,0.3)
+rep(c("Tropics","Temperate","Boreal"),c(3,3,3))
+## Q8
+x <- c(5,3,8,2,9,3,6,9,1,0,2,7)
+sum(x)
+mean(x)
+length(x)
+## Q9
+x <- c(3,9,6,1,9,4,7,8,2,6,3,8,0,2,5)
+sort(x)
+rev(sort(x))
+## Q10 
+## Q10 A
+## setwd gets or sets a working directory. It tells R where it should read and write files from. Ex setting it to the C drive on your PC and a file path to a specific folder
+## arguments get(wd) and setwd(dir)
+## Q10 B 
+## readtable reads a file in table format and then uses the data to create a data frame, with lines and variables set
+## arguments can be file, header, sep or field separator, quote, dec or decimal point, numberals, row.names, col.names, as,is, na.strings, colClasses, nrows, skip, check.names, fill, strip.white, blank.lines.skip, comment.char, allowEscapes, flush, stringsAsFactors, fileEncoding, encoding, text, skipNul, and more. 
+## Q10 C 
+## functions to get or set the names of an object
+## arguments x is an R object, value is a character vector of up to same length as x or NULL, names(x), names(x) <- value 
+## Q10 D
+## attach attaches a database to the R search path, example bringing something into the local network data wise
+## arguments, what, pos or position, name, warn.conflicts 
+## Q10 E 
+## range returns a vector with the minimum and maximum of all given arguments
+## arguments, ... any numeric or character objects, na.rm any NA's omitted, finite, all non-finite elements omitted 
+## Q11
+
+    
